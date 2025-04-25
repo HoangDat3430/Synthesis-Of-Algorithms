0e8ac4fee6cd152cce3df4304a6fd3de136325f3
Dat 5 Ty <hoangmanhdat2000@gmail.com>
2025-04-25 23:47:20 +0700

    điều chỉnh materials

--- Assets/Scripts/Grid/Base/GridBaseData.cs
+++ Assets/Scripts/Grid/Base/GridBaseData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Serializable] public abstract class GridBaseData
 {
     public int edgeSize = 1;
-    public float gridSpacing => edgeSize / 10;
+    public float gridSpacing;
     public int mapWidth = 2;
     public int mapHeight = 4;
     public Material normalMat;
