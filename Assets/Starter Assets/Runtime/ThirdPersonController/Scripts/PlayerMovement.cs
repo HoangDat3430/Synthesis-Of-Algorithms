120212ad138c05d8351082131562a1299f74a3e0
Dat 5 Ty <hoangmanhdat2000@gmail.com>
2025-06-23 23:08:23 +0700

    Import project template về character controller và camera follow (TPS & FPS)

---
+++ Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float walkSpeed = 4f;
+    public float runSpeed = 8f;
+    public float jumpForce = 6f;
+    public float gravity = -9.81f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private Vector3 moveDir = Vector3.zero;
+    private bool isDashing = false;
+    private bool canDash = true;
+    private float dashSpeed = 20f;
+    private float dashDuration = 0.2f;
+    private float dashCooldown = 1f;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            HandleMovement();
+        }
+        else
+        {
+            StartCoroutine(Dash());
+        }
+        HandleJumpAndGravity();
+    }
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        float elapsed = 0f;
+        while (elapsed < dashDuration)
+        {
+            controller.Move(moveDir.normalized * dashSpeed * Time.deltaTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        } 
+        yield return new WaitForSeconds(dashCooldown);
+        isDashing = false;
+        canDash = true;
+    }
+    void HandleMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        // Move relative to current forward direction
+        moveDir = transform.right * moveX + transform.forward * moveZ;
+
+        // Run when holding Left Shift
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        controller.Move(moveDir * speed * Time.deltaTime);
+    }
+
+    void HandleJumpAndGravity()
+    {
+        // Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f; // keep grounded
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity); // jump physics
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
